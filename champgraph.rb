ea211fc3506829138431b2bd804e517d9480109f
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-01-19 21:51:46 +0100

    Changed filenames of graphs.

--- champgraph.rb
+++ champgraph.rb
@@ -12,16 +12,16 @@ d = Graph.new(url)
 champions.keep_if { |c| c.picks + c.bans > 0 }
 
 champions.sort! { |a, b| b.picks+b.bans <=> a.picks+a.bans }
-d.draw(champions, "by_picks_and_bans")
+d.draw(champions, "ByPicksAndBans")
 
 champions.sort! { |a, b| b.picks <=> a.picks }
-d.draw(champions, "by_picks")
+d.draw(champions, "ByPicks")
 
 champions.sort! { |a, b| b.wins <=> a.wins }
-d.draw(champions, "by_wins")
+d.draw(champions, "ByWins")
 
 champions.sort! { |a, b| b.losses <=> a.losses }
-d.draw(champions, "by_losses")
+d.draw(champions, "ByLosses")
 
 def div(x, y)
 	if y == 0 then 
@@ -34,12 +34,12 @@ def div(x, y)
 end
 
 champions.sort! { |a, b| div(b.wins,b.losses) <=> div(a.wins,a.losses) }
-d.draw(champions, "by_win_to_loss_ratio")
+d.draw(champions, "ByWinToLossRatio")
 
 champions.keep_if { |c| c.bans > 0 }
 
 champions.sort! { |a, b| b.bans <=> a.bans }
-d.draw(champions, "by_bans")
+d.draw(champions, "ByBans")
 
-champions.sort! { |a, b| a.picks/a.bans <=> b.picks/b.bans }
-d.draw(champions, "by_picks_to_bans_ratio_desc")
+champions.sort! { |a, b| b.picks/b.bans <=> a.picks/a.bans }
+d.draw(champions, "ByPicksToBansRatio")
