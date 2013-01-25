7b25682dbaa6b7038709c2130ecc8e8874a14ad1
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-01-25 20:08:53 +0100

    Refactored code in champgraph.rb into the Champion class.

--- Champion.rb
+++ Champion.rb
@@ -1,5 +1,6 @@
 class Champion
-	attr_accessor :name, :bans, :picks, :wins, :losses
+	
+	attr_reader :name, :bans, :picks, :wins, :losses 
 
 	def initialize(name, bans, picks, wins, losses)
 		@name = name
@@ -9,8 +10,46 @@ class Champion
 		@losses = losses
 	end
 
+	def picks_and_bans
+		@picks + @bans
+	end
+
+	def win_to_loss_ratio
+		div(@wins, @picks)
+	end
+
+	def picks_to_bans_ratio
+		div(@picks, @bans)
+	end
+
+	def valid
+		@name != ""
+	end
+
+	def were_picked_or_banned
+		@picks + @bans > 0
+	end
+
+	def were_banned
+		@bans > 0
+	end
+
 	def to_s
 		sep = " - "
-		name + sep + bans.to_s + sep + picks.to_s + sep + wins.to_s + sep + losses.to_s
+		@name + sep + @bans.to_s + sep + @picks.to_s + sep + @wins.to_s + sep + @losses.to_s
+	end
+
+	private
+
+	# Ugly hack to avoid division by zero error.
+	def div(x, y)
+		if y == 0 then 
+			return 1000000+x
+		end
+		if x == 0 then
+			return 0-y
+		end
+
+		return x/y
 	end
 end
--- champgraph.rb
+++ champgraph.rb
@@ -2,48 +2,48 @@ require_relative 'Scraper'
 require_relative 'Champion'
 require_relative 'Graph'
 
-s = Scraper.new
 url = ARGV[0]
+
+s = Scraper.new
 title = s.scrape_title(url)
 title = title.gsub(/\s+/, '') # Get rid of whitespace
 title = title.gsub(/\//, '')  # And forward slash
+
+puts "Scraping URL"
 champions = s.scrape_champions(url)
 
-champions.keep_if { |c| c.name != "" }
+champions.keep_if { |c| c.valid }
 
-d = Graph.new(title, url)
-champions.keep_if { |c| c.picks + c.bans > 0 }
+champions.keep_if { |c| c.were_picked_or_banned }
 
-champions.sort! { |a, b| b.picks+b.bans <=> a.picks+a.bans }
-d.draw(champions, "ByPicksAndBans")
+graph = Graph.new(title, url)
 
+puts "Creating image ByPicksAndBans"
+champions.sort! { |a, b| b.picks_and_bans <=> a.picks_and_bans }
+graph.draw(champions, "ByPicksAndBans")
+
+puts "Creating image ByPicks"
 champions.sort! { |a, b| b.picks <=> a.picks }
-d.draw(champions, "ByPicks")
+graph.draw(champions, "ByPicks")
 
+puts "Creating image ByWins"
 champions.sort! { |a, b| b.wins <=> a.wins }
-d.draw(champions, "ByWins")
+graph.draw(champions, "ByWins")
 
+puts "Creating image ByLosses"
 champions.sort! { |a, b| b.losses <=> a.losses }
-d.draw(champions, "ByLosses")
+graph.draw(champions, "ByLosses")
 
-# Hack to avoid division by zero error.
-def div(x, y)
-	if y == 0 then 
-		return 1000000+x
-	end
-	if x == 0 then
-		return 0-y
-	end
-	return x/y
-end
+puts "Creating image ByWinToLossRatio"
+champions.sort! { |a, b| b.win_to_loss_ratio <=> a.win_to_loss_ratio }
+graph.draw(champions, "ByWinToLossRatio")
 
-champions.sort! { |a, b| div(b.wins,b.losses) <=> div(a.wins,a.losses) }
-d.draw(champions, "ByWinToLossRatio")
+champions.keep_if { |c| c.were_banned }
 
-champions.keep_if { |c| c.bans > 0 }
-
+puts "Creating image ByBans"
 champions.sort! { |a, b| b.bans <=> a.bans }
-d.draw(champions, "ByBans")
+graph.draw(champions, "ByBans")
 
-champions.sort! { |a, b| b.picks/b.bans <=> a.picks/a.bans }
-d.draw(champions, "ByPicksToBansRatio")
+puts "Creating image ByPicksToBansRatio"
+champions.sort! { |a, b| b.picks_to_bans_ratio <=> a.picks_to_bans_ratio }
+graph.draw(champions, "ByPicksToBansRatio")
