9ee8667924dca3aec4893c6eb862bebc73fc1821
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-09-14 17:58:14 +0200

    Updated Jobs info.

--- Jobs.rb
+++ Jobs.rb
@@ -1,28 +1,37 @@
 class Jobs
+	def self.to_select_html
+		puts "<select>"
+		jobs.each { |key, job|
+			puts "<option value=\"#{key}\">#{job[:name]}</option>"
+		}
+		puts "</select>"
+	end
+
+
 	def self.jobs
 		eu_lcs_spring_season = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Spring_Season"]
 
-		eu_lcs_spring_playoffs = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Spring_Season"]
+		eu_lcs_spring_playoffs = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Spring_Playoffs"]
 
-		eu_lcs_spring_promotion = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Summer_Promotion"]
+		eu_lcs_summer_promotion = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Summer_Promotion"]
 
 		eu_lcs_summer_season = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Summer_Season"]
 
 		eu_lcs_summer_playoffs = ["http://lol.gamepedia.com/Riot_League_Championship_Series/Europe/Season_3/Picks_and_Bans/Summer_Playoffs"]
 
-		eu_lcs = eu_lcs_spring_season + eu_lcs_spring_playoffs + eu_lcs_spring_promotion + eu_lcs_summer_season + eu_lcs_summer_playoffs
+		eu_lcs = eu_lcs_spring_season + eu_lcs_spring_playoffs + eu_lcs_summer_promotion + eu_lcs_summer_season + eu_lcs_summer_playoffs
 
 		na_lcs_spring_season = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Spring_Season"]
 
 		na_lcs_spring_playoffs = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Spring_Playoffs"]
 
-		na_lcs_spring_promotion = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Summer_Promotion"]
+		na_lcs_summer_promotion = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Summer_Promotion"]
 
 		na_lcs_summer_season = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Summer_Season"]
 
 		na_lcs_summer_playoffs = ["http://lol.gamepedia.com/Riot_League_Championship_Series/North_America/Season_3/Picks_and_Bans/Summer_Playoffs"]
 
-		na_lcs = na_lcs_spring_season + na_lcs_spring_playoffs + na_lcs_spring_promotion + na_lcs_summer_season + na_lcs_summer_playoffs
+		na_lcs = na_lcs_spring_season + na_lcs_spring_playoffs + na_lcs_summer_promotion + na_lcs_summer_season + na_lcs_summer_playoffs
 
 		lcs = eu_lcs + na_lcs
 		
@@ -36,9 +45,9 @@ class Jobs
 				:name => "EU LCS Spring Playoffs",
 				:urls => eu_lcs_spring_playoffs
 			},
-			:eu_lcs_spring_promotion => {
-				:name => "EU LCS Spring Promotion",
-				:urls => eu_lcs_spring_promotion
+			:eu_lcs_summer_promotion => {
+				:name => "EU LCS Summer Promotion",
+				:urls => eu_lcs_summer_promotion
 			},
 			:eu_lcs_summer_season => {
 				:name => "EU LCS Summer Season",
@@ -61,9 +70,9 @@ class Jobs
 				:name => "NA LCS Spring Playoffs",
 				:urls => na_lcs_spring_playoffs
 			},
-			:na_lcs_spring_promotion => {
-				:name => "NA LCS Spring Promotion",
-				:urls => na_lcs_spring_promotion
+			:na_lcs_summer_promotion => {
+				:name => "NA LCS Summer Promotion",
+				:urls => na_lcs_summer_promotion
 			},
 			:na_lcs_summer_season => {
 				:name => "NA LCS Summer Season",
