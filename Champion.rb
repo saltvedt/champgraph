52bbcce7c6f9c9129d469041533ab84df4762abd
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-09-15 00:14:32 +0200

    Added spec for Champion and fixed a number of smaller bugs.

--- Champion.rb
+++ Champion.rb
@@ -1,74 +1,74 @@
 class Champion
-	
-	attr_reader :name, :bans, :picks, :wins, :losses 
+    attr_reader :name, :bans, :picks, :wins, :losses
 
-	def initialize(name, bans, picks, wins, losses)
-		@name = name
-		@bans = bans
-		@picks = picks
-		@wins = wins
-		@losses = losses
-	end
+    def initialize(name, bans, picks, wins, losses)
+        @name = name
+        @bans = bans
+        @picks = picks
+        @wins = wins
+        @losses = losses
+    end
 
-	# Add anothers champions stats to this one 
-	def add(champion)
-		if @name = champion.name
-			@bans 	+= champion.bans
-			@picks 	+= champion.picks
-			@wins 	+= champion.wins
-			@losses += champion.losses
-		end
-	end
+    # Add anothers champions stats to this one
+    def add(champion)
+        if @name = champion.name
+            @bans   += champion.bans
+            @picks  += champion.picks
+            @wins   += champion.wins
+            @losses += champion.losses
+        end
+    end
 
-	def picks_and_bans
-		@picks + @bans
-	end
+    def picks_and_bans
+        @picks + @bans
+    end
 
-	def win_to_loss_ratio
-		ratio(@wins, @losses)
-	end
+    def win_to_loss_ratio
+        Champion::ratio(@wins, @losses)
+    end
 
-	def picks_to_bans_ratio
-		ratio(@picks, @bans)
-	end
+    def picks_to_bans_ratio
+        Champion::ratio(@picks, @bans)
+    end
 
-	def valid
-		@name != ""
-	end
+    def valid
+        @name != "" && @name != nil
+    end
 
-	def were_picked_or_banned
-		@picks + @bans > 0
-	end
+    def were_picked_or_banned
+        @picks + @bans > 0
+    end
 
-	def were_banned
-		@bans > 0
-	end
+    def were_banned
+        @bans > 0
+    end
 
-	def to_s
-		sep = " - "
-		@name + sep + @bans.to_s + sep + @picks.to_s + sep + @wins.to_s + sep + @losses.to_s
-	end
+    def to_s
+        sep = " - "
+        @name + sep + @bans.to_s + sep + @picks.to_s + sep + @wins.to_s + sep + @losses.to_s
+    end
 
-	def to_a
-		[@name, @bans.to_s, @picks.to_s, @wins.to_s, @losses.to_s]
-	end
+    def to_a
+        [@name, @bans.to_s, @picks.to_s, @wins.to_s, @losses.to_s]
+    end
 
-	private
+    # Ugly hack to avoid division by zero
+    def self.ratio(x, y)
+        x = x.to_f
+        y = y.to_f
 
-	# Ugly hack to avoid division by zero
-	def ratio(x, y)
-		if (x == 0) and (y == 0) then
-			return 0
-		end
+        if (x == 0) and (y == 0) then
+            return 0.to_f
+        end
 
-		if (y == 0) then
-			return x+1000
-		end
+        if (y == 0) then
+            return (x+1000).to_f
+        end
 
-		if x == 0 then
-			return 0-y
-		end
+        if x == 0 then
+            return (0-y).to_f
+        end
 
-		return (x/y).to_f
-	end
+        return (x/y).to_f
+    end
 end
--- spec/champion_spec.rb
+++ spec/champion_spec.rb
@@ -1,5 +1,82 @@
 require 'spec_helper'
 
 describe Champion do
-	
+	before(:each) do
+		# Champion.new("name", "bans", "picks", "wins", "losses")
+		@champion = Champion.new("Name", 5, 10, 15, 20)
+		@unused_champion = Champion.new("Name", 0, 0, 0, 0)
+		@invalid_champion = Champion.new(nil, nil, nil, nil, nil)
+	end
+
+	it "#picks_and_bans return the sum of picks and bans" do
+		expect(@champion.picks_and_bans).to eq(15)
+	end
+
+	it "#win_to_loss_ratio should return ratio of win:loss" do
+		expect(@champion.win_to_loss_ratio).to eq(0.75)
+	end
+
+	it "#picks_to_bans_ratio should return ratio of picks:losses" do
+		expect(@champion.picks_to_bans_ratio).to eq(2)
+	end
+
+	it "#valid returns false if champion data is invalid" do
+		expect(@invalid_champion.valid).to eq(false)
+	end
+
+	it "#were_picked_or_banned returns true or false depending on whether the champion were picked or banned" do
+		expect(@champion.were_picked_or_banned).to eq(true)
+		expect(@unused_champion.were_picked_or_banned).to eq(false)
+	end
+
+	it "#were_banned returns true or false depending on wheter the champion were banned or not" do
+		expect(@champion.were_banned).to eq(true)
+		expect(@unused_champion.were_banned).to eq(false)
+	end
+
+	it "#to_s returns a string representation of the champion" do
+		expect(@champion.to_s).to eq("Name - 5 - 10 - 15 - 20")
+	end
+
+	it "#to_a returns an array with the champion data in string format" do
+		expect(@champion.to_a).to eq(["Name", 5.to_s, 10.to_s, 15.to_s, 20.to_s])
+	end
+
+	describe ".ratio" do
+		it "returns a float" do
+			Champion::ratio(1,1).should be_a_kind_of(Float)
+			Champion::ratio(1,0).should be_a_kind_of(Float)
+			Champion::ratio(0,1).should be_a_kind_of(Float)
+			Champion::ratio(5,3).should be_a_kind_of(Float)
+			Champion::ratio(3,5).should be_a_kind_of(Float)
+		end
+
+		it "returns 1 when dividend and numerator are equal" do
+			expect(Champion::ratio(1,1)).to eq(1.to_f)
+			expect(Champion::ratio(5,5)).to eq(1.to_f)
+			expect(Champion::ratio(10,10)).to eq(1.to_f)
+		end
+
+		it "returns 0 when dividend and numerator are zero" do
+			expect(Champion::ratio(0,0)).to eq(0.to_f)
+		end
+
+		it "returns 0.5 when dividend is half of numerator" do
+			expect(Champion::ratio(5,10)).to eq(0.5)
+			expect(Champion::ratio(20,40)).to eq(0.5)
+			expect(Champion::ratio(1,2)).to eq(0.5)
+		end
+
+		it "returns 0.333.. when dividend is a third of numerator" do
+			expect(Champion::ratio(1,3)).to eq(1.0/3)
+			expect(Champion::ratio(20,60)).to eq(1.0/3)
+			expect(Champion::ratio(30,90)).to eq(1.0/3)
+		end
+
+		it "returns 0.75 when dividend is three quarters of numerator" do
+			expect(Champion::ratio(3,4)).to eq(0.75)
+			expect(Champion::ratio(15,20)).to eq(0.75)
+			expect(Champion::ratio(75,100)).to eq(0.75)
+		end
+	end
 end
